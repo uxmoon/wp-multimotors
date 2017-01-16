59a648740e4b02d8760c50accc788025dfb24b75
Martin Luna <martin.luna@rga.com>
2017-01-16 14:14:08 -0300

    updated 404 page markup

--- inc/tweaks.php
+++ inc/tweaks.php
@@ -24,16 +24,3 @@ add_action('init', 'modify_jquery');
 
 // remove version from head
 remove_action('wp_head', 'wp_generator');
-
-// remove version from rss
-add_filter('the_generator', '__return_empty_string');
-
-// remove version from scripts and styles
-function quechalen_remove_version_scripts_styles($src) {
-  if (strpos($src, 'ver=')) {
-    $src = remove_query_arg('ver', $src);
-  }
-  return $src;
-}
-add_filter('style_loader_src', 'quechalen_remove_version_scripts_styles', 9999);
-add_filter('script_loader_src', 'quechalen_remove_version_scripts_styles', 9999);
